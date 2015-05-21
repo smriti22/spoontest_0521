bcf6c18c228d32c95344f08f48b71cda383359a8
unknown <in8kumni@DKV1106.corp.lego.com>
2015-05-21 11:48:41 +0530

    :second run

---
+++ Spoon_RnD_V2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace Spoon_RnD_V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FirefoxProfile profile = new FirefoxProfile();
+            Proxy proxy = new Proxy();
+            proxy.IsAutoDetect = true;
+            profile.SetProxyPreferences(proxy);
+
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "https://www.google.com";
+
+        }
+    }
+}
